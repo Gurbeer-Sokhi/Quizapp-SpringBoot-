b6b37c556fb5c5ac7bb6f943eb970c4efea5426e
Gurbeer-Sokhi <gsokhi@stevens.edu>
2023-08-17 22:59:09 -0400

    setup repository,controller,service and model.

--- src/main/java/com/Gurbeer/quizapp/QuestionController.java
+++ src/main/java/com/Gurbeer/quizapp/QuestionController.java
@@ -1,16 +1,24 @@
 package com.Gurbeer.quizapp;
 
+import com.Gurbeer.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
 
+
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
-        return "Hi, These are your questions";
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
     }
 
 }
---
+++ src/main/java/com/Gurbeer/quizapp/service/QuestionService.java
@@ -0,0 +1,20 @@
+package com.Gurbeer.quizapp.service;
+
+import com.Gurbeer.quizapp.Question;
+import com.Gurbeer.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+
+    }
+}
